30d8c6d06281f08555a16bbbc5fcda7de81b0713
Prakash <vinprakash@deloitte.com>
2019-08-25 14:20:28 +0530

    added dist

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const request = require('request');
 const app = express();
 
 app.use(cors());
-
+app.use(express.static('public'));
 app.get('/getAllEvents', (req, res) => {
 	request(
 		`https://in.bookmyshow.com/serv/getData?cmd=GETTRAILERS&mtype=cs`,
